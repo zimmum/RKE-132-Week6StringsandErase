55faf13c04e065cc3008dc4fe24d4b47088c3efb
zimmu. <mzimmer@tktk.ee>
2023-10-02 09:29:59 +0300

    add week6StringsandErase code

---
+++ Arreywithnumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[10];
+Random rand = new Random();
+
+int sum = 0;
+
+//populating the array
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rand.Next(1, numbers.Length + 1);
+    sum = sum + numbers[i];
+}
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine($"Total: {sum}");
---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "asian takeaway" };
+
+Random rand = new Random();
+
+int randomIndex = rand.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
